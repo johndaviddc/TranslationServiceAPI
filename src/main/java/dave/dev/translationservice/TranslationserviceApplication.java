b56cdede4e3325ab815da47ace5b9f8686e13cb5
John David Dela Cruz <johndaviddc1@gmail.com>
2024-01-26 16:32:54 +0800

    translation service API

---
+++ src/main/java/dave/dev/translationservice/TranslationserviceApplication.java
@@ -0,0 +1,13 @@
+package dave.dev.translationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslationserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dave/dev/translationservice/TranslationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.translationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
